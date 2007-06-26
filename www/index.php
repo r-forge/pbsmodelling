fb7b8e6ab48b6fe503468a136bb42c399f3c7024
alex (Alex Couture-Beil) <rforge@mofo.ca>
2007-06-26 17:09:58 +0000

    changed website welcome page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pbsmodelling@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,9 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> Hello World </p>
+
+<p> I am now in the process of merging code over to r-forge (here). <br/>-Alex</p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
